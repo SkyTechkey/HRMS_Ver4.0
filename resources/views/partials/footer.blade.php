fa9789dadb342fe31a3f91e9dfeabe2bec3e6fd1
SKYTECH <vinh.hv@skytechkey.com>
2021-06-08 21:13:10 +0700

    no message

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,10 @@
+<!-- BEGIN FOOTER -->
+<div class="page-footer">
+    <div class="page-footer-inner pull-right">Phần mềm quản trị nhân sự - v1.0.0
+    </div>
+    
+    <div class="scroll-to-top">
+        <i class="icon-arrow-up"></i>
+    </div>
+</div>
+<!-- END FOOTER -->
